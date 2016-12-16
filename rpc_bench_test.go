b2e79a2c59f3042fd0942b31e79ebf6d4f806647
zkfy <lib4net@163.com>
2016-12-16 16:52:51 +0800

    .

---
+++ rpc_bench_test.go
@@ -0,0 +1,59 @@
+package grpc4ars
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/qxnw/grpc4ars/client"
+	"github.com/qxnw/grpc4ars/server"
+)
+
+func BenchmarkItems(t *testing.B) {
+	svr := server.NewServer(func(session string, svs string, data string) (status int, result string, err error) {
+		status = 100
+		result = svs
+		return
+	})
+	go func() {
+		if err := svr.Start(":10160"); err != nil {
+			t.Error(err)
+		}
+	}()
+
+	client := client.NewClient(":10160")
+	if e := client.Connect(); e != nil {
+		t.Error(e)
+		return
+	}
+	fmt.Println("s:", time.Now())
+	s, result, err := client.Request("123455666", "svname", "{}")
+	//	fmt.Println(svname)
+	if err != nil {
+		t.Error(err)
+	}
+	if s != 100 || result != "svname" {
+		t.Error("数据有误")
+	}
+	fmt.Println("e:", time.Now())
+
+	mu := sync.WaitGroup{}
+	for i := 0; i < t.N; i++ {
+		mu.Add(1)
+		go func(i int) {
+			svname := fmt.Sprintf("svs:%d", i)
+			s, result, err := client.Request("123455666", svname, "{}")
+			//	fmt.Println(svname)
+			if err != nil {
+				t.Error(err)
+			}
+			if s != 100 || result != svname {
+				t.Error("数据有误")
+			}
+			mu.Done()
+
+		}(i)
+	}
+	mu.Wait()
+}
